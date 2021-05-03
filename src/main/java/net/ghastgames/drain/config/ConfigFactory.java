8b3c088f246aa89e38d2fceff88143fa31881b6d
EinGriefer <simplecontent@outlook.com>
2021-05-03 10:30:25 +0200

    Update ConfigFactory.java

--- src/main/java/net/ghastgames/drain/config/ConfigFactory.java
+++ src/main/java/net/ghastgames/drain/config/ConfigFactory.java
@@ -8,4 +8,21 @@ public class ConfigFactory {
     public ConfigFactory(JavaPlugin plugin) {
         this.plugin = plugin;
     }
+
+    /**
+     * Adds a default value to the plugin's configuration
+     *
+     * @param path The path
+     *             Example:
+     *             "name" would result in a configuration like this:
+     *             name: EinGriefer
+     *             "permission.command" would result in a configuration like this:
+     *             permission:
+     *               command: any.thing
+     * @param value The default value
+     */
+    public ConfigFactory addDefaultValue(String path, Object value) {
+        this.plugin.getConfig().addDefault(path, value);
+        return this;
+    }
 }
