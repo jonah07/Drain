72dd43d55ba53d75380a3039fa1aa6d0c2082f1c
EinGriefer <simplecontent@outlook.com>
2021-05-03 10:07:49 +0200

    Create ConfigFactory.java

---
+++ src/main/java/net/ghastgames/drain/config/ConfigFactory.java
@@ -0,0 +1,11 @@
+package net.ghastgames.drain.config;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigFactory {
+    private JavaPlugin plugin;
+
+    public ConfigFactory() {
+
+    }
+}
