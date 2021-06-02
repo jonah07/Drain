b0f08d703083ea2b23d8b440a0766e6fe4e7e202
EinGriefer <simplecontent@outlook.com>
2021-06-02 08:33:22 +0200

    Working on tabcompletion

---
+++ src/main/java/net/ghastgames/drain/tabcomplete/TabCompleteUtils.java
@@ -0,0 +1,4 @@
+package net.ghastgames.drain.tabcomplete;
+
+public class TabCompleteUtils {
+}
--- src/main/java/net/ghastgames/drain/tabcomplete/TabCompleterImpl.java
+++ src/main/java/net/ghastgames/drain/tabcomplete/TabCompleterImpl.java
@@ -1,15 +1,39 @@
 package net.ghastgames.drain.tabcomplete;
 
+import net.ghastgames.drain.command.BukkitCommandRegistry;
+import net.ghastgames.drain.command.DrainCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class TabCompleterImpl implements TabCompleter {
 
     @Override
-    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
-        return null;
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String alreadyTyped, String[] args) {
+        DrainCommand drainCommand = BukkitCommandRegistry.getCommand(command);
+
+        // Return when there are no tab complete options
+        if(drainCommand.getTabCompleteOptions().size() == 0) {
+            return Collections.emptyList();
+        }
+
+        List<TabCompleteOption> options = drainCommand.getTabCompleteOptions();
+
+        Stream<TabCompleteOption> optionStream = options.stream();
+
+        optionStream = optionStream.filter(new Predicate<TabCompleteOption>() {
+            @Override
+            public boolean test(TabCompleteOption tabCompleteOption) {
+                return tabCompleteOption.getCondition().isEmpty() || tabCompleteOption.getCondition().equalsIgnoreCase(alreadyTyped);
+            }
+        });
+
+        return Arrays.asList();
     }
 }
