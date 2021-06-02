c958b0ef2923b5ef0c02e5d2cd17c687f3f957e4
EinGriefer <simplecontent@outlook.com>
2021-06-02 08:26:59 +0200

    Added BukkitCommandRegistry#getCommand

--- src/main/java/net/ghastgames/drain/command/BukkitCommandRegistry.java
+++ src/main/java/net/ghastgames/drain/command/BukkitCommandRegistry.java
@@ -1,7 +1,9 @@
 package net.ghastgames.drain.command;
 
 import net.ghastgames.drain.annotations.PluginCommand;
+import net.ghastgames.drain.tabcomplete.TabCompleterImpl;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,6 +12,8 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class BukkitCommandRegistry {
     private static List<DrainCommand> commands = new ArrayList<>();
@@ -47,9 +51,31 @@ public class BukkitCommandRegistry {
     public static void registerCommand(DrainCommand drainCommand, JavaPlugin plugin) {
         commands.add(drainCommand);
         plugin.getCommand(drainCommand.getCommand()).setExecutor((sender, cmd, s, args) -> true);
+        plugin.getCommand(drainCommand.getCommand()).setTabCompleter(new TabCompleterImpl());
     }
 
     public static List<DrainCommand> getCommands() {
         return commands;
+    }
+
+    public static DrainCommand getCommand(String command) {
+        List<DrainCommand> result = commands.stream().filter(new Predicate<DrainCommand>() {
+            @Override
+            public boolean test(DrainCommand drainCommand) {
+                return drainCommand.getCommand().equalsIgnoreCase(command);
+            }
+        }).collect(Collectors.toList());
+
+        if(result.size() == 1) {
+            return result.get(0);
+        } else if (result.size() > 1) {
+            throw new RuntimeException();
+        } else {
+            throw new CommandException(String.format("Command %s not found", command));
+        }
+    }
+
+    public static DrainCommand getCommand(Command command) {
+        return getCommand(command.getName());
     }
 }
