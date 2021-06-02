0006a1e5cc5a0c8288636b3c34d7b5537dd8f266
EinGriefer <simplecontent@outlook.com>
2021-06-02 08:36:06 +0200

    Finished TabComplete UNTESTED

--- src/main/java/net/ghastgames/drain/tabcomplete/TabCompleteUtils.java
+++ src/main/java/net/ghastgames/drain/tabcomplete/TabCompleteUtils.java
@@ -1,4 +1,21 @@
 package net.ghastgames.drain.tabcomplete;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TabCompleteUtils {
+
+    /**
+     * Converts a List of TabCompleteOptions into a string list
+     *
+     * @param tabCompleteOptions The list to be converted
+     * @return The string list containing all options
+     */
+    public static List<String> convertTabCompleteOptionsToString(List<TabCompleteOption> tabCompleteOptions) {
+        List<String> returnList = new ArrayList<>();
+
+        tabCompleteOptions.forEach((option) -> returnList.add(option.getOption()));
+
+        return returnList;
+    }
 }
--- src/main/java/net/ghastgames/drain/tabcomplete/TabCompleterImpl.java
+++ src/main/java/net/ghastgames/drain/tabcomplete/TabCompleterImpl.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TabCompleterImpl implements TabCompleter {
@@ -34,6 +35,6 @@ public class TabCompleterImpl implements TabCompleter {
             }
         });
 
-        return Arrays.asList();
+        return TabCompleteUtils.convertTabCompleteOptionsToString(optionStream.collect(Collectors.toList()));
     }
 }
