4338a77fcad95e2443a2155b8b7c2d932c8b366e
EinGriefer <simplecontent@outlook.com>
2021-06-20 18:01:00 +0200

    Added private constructor

--- src/main/java/net/ghastgames/drain/command/argchecker/CommandArgsChecker.java
+++ src/main/java/net/ghastgames/drain/command/argchecker/CommandArgsChecker.java
@@ -2,6 +2,12 @@ package net.ghastgames.drain.command.argchecker;
 
 // https://github.com/EinGriefer/Drain/issues/12
 public class CommandArgsChecker {
-    
-    
+    private static CommandArgsChecker instance;
+
+    private CommandArgsChecker() {}
+
+    public static CommandArgsChecker getInstance() {
+        if(instance == null) instance = new CommandArgsChecker();
+        return instance;
+    }
 }
